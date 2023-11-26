53c640b7e24fc3504631346c2d11c2b68a6954e5
Atish Panday <atishpanday@Atishs-MacBook-Air.local>
2023-11-26 01:43:38 -0800

    successfully added tile transitions

--- src/utils/transformTile.ts
+++ src/utils/transformTile.ts
@@ -1,17 +1,17 @@
-export const transformTile = (dir: string, row: number, col: number, rIndex: number, newTile: boolean): string => {
-    if(newTile) {
+export const transformTile = (dir: string, transitionVal: number, newTile: boolean): string => {
+    if (newTile) {
         return 'scale(0)';
     }
     else {
-        switch(dir) {
-            case 'up':
-                return `translateY(${(3 - row) * 100}px)`;
-            case 'down':
-                return `translateY(-${row * 100}px)`;
-            case 'left':
-                return `translateX(${(3 - col) * 100}px)`;
-            case 'right':
-                return `translateX(-${col * 100}px)`;
+        switch (dir) {
+            case 'ArrowUp':
+                return `translateY(${transitionVal * 100}px)`;
+            case 'ArrowDown':
+                return `translateY(-${transitionVal * 100}px)`;
+            case 'ArrowLeft':
+                return `translateX(${transitionVal * 100}px)`;
+            case 'ArrowRight':
+                return `translateX(-${transitionVal * 100}px)`;
             default:
                 return 'translate(0)';
         }
